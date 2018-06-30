b5493e4031725596225b9d07ce6c6500fc2a7285
Tristan Goss <tristanjdgoss@gmail.com>
2018-06-30 13:10:19 +0100

    attempted to get setup.py to properly install templates and static

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
+
+def package_files(directory):
+    """see https://stackoverflow.com/questions/27664504/how-to-add-package-data-recursively-in-python-setup-py"""
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+static_files = package_files('MediaTracker/static')
+template_files = package_files('MediaTracker/templates')
+
 
 setup(name='MediaTracker',
       version='1.10',
@@ -11,7 +26,8 @@ setup(name='MediaTracker',
           'sqlalchemy-migrate',
           'flask-wtf',
       ],
+      package_data={'': static_files + template_files},
       author_email='sarahbeharry@hotmail.com',
       license='MIT',
-      packages=find_packages(),
+      packages=['MediaTracker'],
       )
